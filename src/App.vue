866dcf6d7f129627828fe0c8b6678669658f46b9
Slava <krylov_s10@mail.ru>
2024-12-31 06:25:17 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="landing">
+    <header class="header">
+      <h1>Вика, с новым годом!</h1>
+      <p>
+        Этот сайт персонально для тебя и хочется поздравить тебя с новым годом,
+        ниже мои друзья записали тебе видео)
+      </p>
+    </header>
+    <main class="main-content">
+      <video class="video" src="../public/IMG_4993.MP4" controls></video>
+      <Button
+        as="a"
+        href="../public/IMG_4993.MP4"
+        download="IMG_4993.mp4"
+        severity="secondary"
+        class="download-button"
+      >
+        Скачать видео
+      </Button>
+      <p>
+        Если будут проблемы,
+        <a href="https://t.me/slavahawk" target="_blank">напиши мне</a>, скину
+        так)
+      </p>
+      <h2 class="gift-header">Ниже твой скромный подарочек!</h2>
+      <Button
+        as="a"
+        href="https://ga.gift/ru/cb5db3a35f27464fbb15b16967c32ea7"
+        target="_blank"
+        severity="secondary"
+        class="gift-button"
+        >Забрать подарок
+      </Button>
+    </main>
+  </div>
+</template>
+
+<script setup>
+import { Button } from "primevue";
+</script>
+
+<style scoped>
+.landing {
+  background-color: #fcf8f5;
+  color: #1f4f83;
+  padding: 20px;
+  text-align: center;
+}
+
+.header {
+  padding: 20px;
+  background-color: #ffcccc;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.main-content {
+  margin: 20px 0;
+}
+
+.video {
+  width: 100%;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.download-button,
+.gift-button {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+.gift-header {
+  margin-top: 40px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import './assets/main.css'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+app.mount("#app")
+
+
+
+
+
+
+
